3663eeadf472bbefe4c400386d3ab9657b18970e
José Rey Arijón <jose.reyarijon@WIRTZ.LOCAL>
2022-12-13 22:15:20 +0100

    created function is_inside, incorrect solution

--- 04/clean_up.py
+++ 04/clean_up.py
@@ -1,5 +1,13 @@
 import os, sys
 
+
+def is_inside(my_list1, my_list2):
+    bools1 = list(map(lambda x: x in my_list2, my_list1))
+    bools2 = list(map(lambda x: x in my_list1, my_list2))
+    if False not in bools1 or  False not in bools2: return 1
+    else:                                           return 0
+
+
 current_dir = os.getcwd()
 source = current_dir + "/04/assignment_pairs.txt"
 
@@ -10,12 +18,9 @@ i = 0
 try:
     for line in fr:
         assignment = line.strip("\n").split(",")
-        pair1, pair2 = assignment[0].split("-"), assignment[1].split("-")
-        elf_assignment_range1 = [n for n in range(int(pair1[0]), int(pair1[1]))]
-        elf_assignment_range2 = [n for n in range(int(pair2[0]), int(pair2[1]))]
-        coincidences = list(map(lambda x : x in elf_assignment_range2, elf_assignment_range1))
-        if False not in coincidences and len(coincidences) == len(elf_assignment_range2): i += 1
-
+        pair1 = list(map(lambda x: int(x), assignment[0].split("-")))
+        pair2 = list(map(lambda x: int(x), assignment[1].split("-")))
+        i += is_inside(range(int(pair1[0]), pair1[1]), range(pair2[0],pair2[1]))
     print(i)
 except IOError:
     print("Input/output issues")
